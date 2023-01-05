4b1117fba194f2523ffb622cd283d7e77d7bb82e
biscu <biscu@live.it>
2023-01-05 14:11:18 +0100

    fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 import './Header.css';
+import { motion } from "framer-motion";
 
 export const Header = (props) => {
     return (
         <div className="header-container">
-            <h1>{ props.header }</h1>
+            <div className="header-wrapper">
+                <motion.h1
+                initial={{ 
+                    y: 120, 
+                }}
+                animate={{ 
+                    y: 0, 
+                }}
+                transition={{
+                    duration: 1,
+                    delay: 0.3,
+                    ease: "easeOut",
+                }}
+                >
+                { props.header }
+                </motion.h1>
+            </div>
         </div>
     )
 }
